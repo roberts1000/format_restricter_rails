6051dc2914ffe8dd8c2e96ba1620dd88eec29d4b
Roberts <roberts@corlewsolutions.com>
2016-05-11 22:58:51 -0400

    Forward options to before_action

--- lib/format_restricter_rails/includes.rb
+++ lib/format_restricter_rails/includes.rb
@@ -4,27 +4,23 @@ module FormatRestricterRails
     def self.included(base)
       base.extend(ClassMethods)
       base.instance_eval do
-        private_class_method :normalize_option
+        private_class_method :register_before_action
       end
     end
 
     module ClassMethods
       def restrict_formats_to(*args)
         options = args.extract_options!
-        except_list = normalize_option(options, :except)
-        only_list = normalize_option(options, :only)
-        allowed_formats = args
-        before_action(only: only_list, except: except_list) do |controller|
-         	unless allowed_formats.include?(request.format.symbol)
+        allowed_formats = args.collect {|e| e.to_sym}
+        register_before_action(options, allowed_formats)
+      end
+
+      def register_before_action(options, allowed_formats)
+        before_action(options) do |controller|
+          unless allowed_formats.include?(request.format.symbol)
             render nothing: true, status: 406
           end
         end
-      end
-
-      def normalize_option(options, key)
-        value = options.fetch(key, [])
-        value = [value] unless value.is_a?(Array)
-        value.collect {|e| e.to_sym}      
       end
     end
   end
